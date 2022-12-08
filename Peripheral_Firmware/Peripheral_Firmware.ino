cc763635a87a0c649bd079a34889539542aa79cc
Eric Cordts <cordts.e@northeastern.edu>
2022-12-07 22:36:26 -0500

    Connections working between main and anklet:
    Also fixed output code

--- Peripheral_Firmware/Peripheral_Firmware.ino
+++ Peripheral_Firmware/Peripheral_Firmware.ino
@@ -1,18 +1,21 @@
-// Anklet Arduino Program 
+// Anklet Arduino Program
 
 #include <ArduinoBLE.h>
 
-#define BLE_UUID_BEACON_SERVICE_DEVICE1           "2a675dfb-a1b0-4c11-9ad1-031a84594196"
-#define BLE_UUID_BEACON_SERVICE_DEVICE2           "ae7a527a-64f7-11ed-9022-0242ac120002"
-#define BLE_UUID_BEACON_SERVICE_DEVICE3           "b52a20c8-64f7-11ed-9022-0242ac120002"
- 
-#define BLE_DEVICE_NAME                           "Arduino Nano 33 BLE (Central)"
-#define BLE_LOCAL_NAME                            "Arduino 1 (Nano 33 BLE) (Central)"
-#define BLE_LED_PIN                               LED_BUILTIN
+#define BLE_UUID_BEACON_SERVICE_DEVICE1 "2a675dfb-a1b0-4c11-9ad1-031a84594196"
+#define BLE_UUID_BEACON_SERVICE_DEVICE2 "ae7a527a-64f7-11ed-9022-0242ac120002"
 
-const int threshold_count = 100; 
+#define BLE_DEVICE_NAME "Anklet"
+#define BLE_LOCAL_NAME "Anklet (Nano 33 BLE)"
+#define BLE_UUID_CHAR "315f50e2-55c9-4b10-8b46-6c66957b4d98"
+
+#define BLE_UUID_ANKLET_SAMPLE "1e0f9d07-42fe-4b48-b405-38374e5f2d97"
+
+#define BLE_LED_PIN LED_BUILTIN
+
+const int threshold_count = 100;
 const int comparison_count = 5;
-const float threshold_factor = 0.75;
+const float threshold_factor = 0.80;
 const float nominal_factor = 0.88;
 
 int nominalDeviceRssi1;
@@ -23,9 +26,11 @@ int threshold2;
 
 bool stepDetected;
 int deviceStepDetected;
+byte stepValue;
 
 // main controller
-BLEDevice mainController;
+BLEDevice mainController_;
+BLECharacteristic stepCharacteristic_;
 
 //function declarations:
 bool setupBleMode();
@@ -34,37 +39,28 @@ void bleConnectHandler(BLEDevice central);
 void bleDisconnectHandler(BLEDevice central);
 
 void setup() {
-  Serial.begin(9600); 
-  while(!Serial);
-  
-  // Pin Outputs 
+  Serial.begin(9600);
+  //while(!Serial);
+
+  // Pin Outputs
   pinMode(LED_BUILTIN, OUTPUT);
 
   // Check Initialization of BLE service
-  if (!setupBleCentral())
-  {
+  if (!setupBleCentral()) {
     Serial.println(F("Failed to initialize BLE"));
     while (1);
-  }
-  else
-  {
+  } else {
     Serial.println(F("BLE initialized. Waiting for client connection"));
   }
 
-  /*
-  connectToDevice(BLE_UUID_BEACON_SERVICE_DEVICE2, beacon2);
-  threshold_2 = threshold_calc(threshold_count, beacon2, nominalDeviceRssi2);
-  Serial.println(F("Threshold 2: "));
-  Serial.println(threshold_2);
+  // First connect to mainController before getting threshold values
+  while(!mainController_)
+  {
+    connectToMainController(mainController_, stepCharacteristic_);
+  }
+  Serial.println("Connected to main controller");
+  delay(100);
   
-
-  // Connect to Peripheral 1 and calculate the threshold
-  connectToDevice(BLE_UUID_BEACON_SERVICE_DEVICE1, beacon1);
-  threshold_1 = threshold_calc(threshold_count, beacon1, nominalDeviceRssi1);
-  Serial.println(F("Threshold 1: "));
-  Serial.println(threshold_1);
-  */
-
   nominalDeviceRssi1 = getDeviceRssi(10, BLE_UUID_BEACON_SERVICE_DEVICE1);
   threshold1 = nominalDeviceRssi1 * threshold_factor;
   Serial.print("Device 1 threshold:");
@@ -73,11 +69,15 @@ void setup() {
   threshold2 = nominalDeviceRssi2 * threshold_factor;
   Serial.print("Device 2 threshold:");
   Serial.println(threshold2);
+
+  stepCharacteristic_.writeValue((uint8_t)(1<<5));
   stepDetected = false;
   deviceStepDetected = 0;
+  
 }
 
 void loop() {
+  
   int device1Reading = -1000;
   int device2Reading = -1000;
   if((!stepDetected) || (stepDetected && deviceStepDetected == 1))
@@ -97,12 +97,15 @@ void loop() {
   {
     stepDetected = true;
     deviceStepDetected = 1;    
+    stepCharacteristic_.writeValue((uint8_t)(1<<0));
     Serial.println("Step detected Device 1");
   }
   else if((device2Reading > threshold2) && !stepDetected)
   {
     stepDetected = true;
     deviceStepDetected = 2;
+    stepValue = 0x02;
+    stepCharacteristic_.writeValue((uint8_t)(1<<1));
     Serial.println("Step detected Device 2");
   }
 
@@ -118,6 +121,7 @@ void loop() {
         // reset stepDetected and deviceStepDetected to default values
         stepDetected = false;
         deviceStepDetected = 0;
+        stepCharacteristic_.writeValue((uint8_t)(1<<6));
         Serial.println("Step cleared");
       }
     }
@@ -128,65 +132,121 @@ void loop() {
         // reset stepDetected and deviceStepDetected to default values
         stepDetected = false;
         deviceStepDetected = 0;
+        stepCharacteristic_.writeValue((uint8_t)(1<<6));
         Serial.println("Step cleared");
       }
     }
   }
-  
 }
 
-bool setupBleCentral()
+void connectToMainController(BLEDevice& mainController, BLECharacteristic& stepCharacteristic)
 {
-  if (!BLE.begin())
+  Serial.println("- Discovering mainController device...");
+
+  do {
+    BLE.scanForUuid(BLE_UUID_ANKLET_SAMPLE);
+    mainController = BLE.available();
+  } while (!mainController);
+
+  if (mainController) {
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(mainController.address());
+    Serial.print("* Device name: ");
+    Serial.println(mainController.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(mainController.advertisedServiceUuid());
+    Serial.println(" ");
+    BLE.stopScan();
+
+    Serial.println("- Connecting to peripheral device...");
+
+    if (mainController.connect()) {
+      Serial.println("* Connected to peripheral device!");
+      Serial.println(" ");
+    } else {
+      Serial.println("* Connection to peripheral device failed!");
+      Serial.println(" ");
+      return;
+    }
+
+    Serial.println("- Discovering peripheral device attributes...");
+    if (mainController.discoverAttributes()) {
+      Serial.println("* Peripheral device attributes discovered!");
+      Serial.println(" ");
+    } else {
+      Serial.println("* Peripheral device attributes discovery failed!");
+      Serial.println(" ");
+      mainController.disconnect();
+      return;
+    }
+
+    stepCharacteristic = mainController.characteristic(BLE_UUID_CHAR);
+
+    if (!stepCharacteristic) {
+      Serial.println("* Peripheral device does not have gesture_type characteristic!");
+      mainController.disconnect();
+      return;
+    } else if (!stepCharacteristic.canWrite()) {
+      Serial.println("* Peripheral does not have a writable gesture_type characteristic!");
+      mainController.disconnect();
+      return;
+    }
+  }
+}
+
+void writeToStepCharacteristic(uint8_t valueToWrite)
+{
+  if(mainController_.connected())
   {
+    stepCharacteristic_.writeValue(valueToWrite);
+  }  
+}
+
+bool setupBleCentral() {
+  if (!BLE.begin()) {
     return false;
   }
 
   Serial.println(F("Set device and local name"));
-  //set device and local name 
+  //set device and local name
   BLE.setDeviceName(BLE_DEVICE_NAME);
   BLE.setLocalName(BLE_LOCAL_NAME);
-  
+
   BLE.setEventHandler(BLEConnected, bleConnectHandler);
   BLE.setEventHandler(BLEDisconnected, bleDisconnectHandler);
   BLE.advertise();
   return true;
 }
 
-void bleConnectHandler(BLEDevice central)
-{
+void bleConnectHandler(BLEDevice central) {
   digitalWrite(BLE_LED_PIN, HIGH);
   Serial.print(F("Connected to central: "));
   Serial.println(central.address());
 }
 
-void bleDisconnectHandler(BLEDevice central)
-{
-  digitalWrite (BLE_LED_PIN, LOW);
+void bleDisconnectHandler(BLEDevice central) {
+  digitalWrite(BLE_LED_PIN, LOW);
   Serial.print(F("Disconnected from central: "));
   Serial.println(central.address());
 }
 
-int threshold_calc(int count, BLEDevice& device, int& nominalDeviceRssi)
-{
+int threshold_calc(int count, BLEDevice& device, int& nominalDeviceRssi) {
   nominalDeviceRssi = averageRssiForComparison(threshold_count, device);
   return threshold_factor * (nominalDeviceRssi);
 }
 
-void connectToDevice(String uuid, BLEDevice& device)
-{
+void connectToDevice(String uuid, BLEDevice& device) {
   Serial.print("Scanning for UUID: ");
   Serial.println(uuid);
-  while(!device.connected())
-  {
+  while (!device.connected()) {
     BLE.scanForUuid(uuid);
     device = BLE.available();
-    if(device)
-    {
+    if (device) {
       Serial.println(device.localName());
       Serial.println(device.advertisedServiceUuid());
       device.connect();
-      BLE.stopScan();        
+      BLE.stopScan();
     }
   }
 }
@@ -227,50 +287,42 @@ void checkBeacon2Rssi(BLEDevice& device)
   }
 }*/
 
-int averageRssiForComparison(int count, BLEDevice& device)
-{
+int averageRssiForComparison(int count, BLEDevice& device) {
   int i = 0;
   int sum = 0;
-  while (i < count){
-    if (device.connected())
-    {
+  while (i < count) {
+    if (device.connected()) {
       int rssiVal = device.rssi();
-      if(rssiVal != 127 && rssiVal != 0)
-      {
+      if (rssiVal != 127 && rssiVal != 0) {
         //Serial.println(rssiVal);
         sum += device.rssi();
         i += 1;
       }
     }
   }
-  return sum/count;
+  return sum / count;
 }
 
-int getDeviceRssi(int checkCount, String uuid)
-{
+int getDeviceRssi(int checkCount, String uuid) {
   int i = 0;
   int sum = 0;
   BLEDevice device;
-  while(!device)
-  {
+  while (!device) {
     BLE.scanForUuid(uuid);
     device = BLE.available();
-    if(device)
-    {
+    if (device) {
       //Serial.println(device.localName());
       //Serial.println(device.advertisedServiceUuid());
       BLE.stopScan();
 
-      while (i < checkCount)
-      {
+      while (i < checkCount) {
         int rssiVal = device.rssi();
-        if(rssiVal != 127 && rssiVal != 0)
-        {
+        if (rssiVal != 127 && rssiVal != 0) {
           sum += device.rssi();
           i += 1;
         }
       }
     }
   }
-  return sum/checkCount;
+  return sum / checkCount;
 }
--- Step_monitor/Step_monitor.ino
+++ Step_monitor/Step_monitor.ino
@@ -1,18 +1,27 @@
 #include <ArduinoBLE.h>
 
-#define BLE_UUID_CHAR                           "315f50e2-55c9-4b10-8b46-6c66957b4d98" //Characteristic UUID
-#define BLE_UUID_ANKLET_SAMPLE                  "2a675dfb-a1b0-4c11-9ad1-031a84594196" //Device UUID
+#define BLE_UUID_CHAR "315f50e2-55c9-4b10-8b46-6c66957b4d98"           //Characteristic UUID
+#define BLE_UUID_ANKLET_SAMPLE "1e0f9d07-42fe-4b48-b405-38374e5f2d97"  //Device UUID
+
+BLEService Arduino_measurements(BLE_UUID_ANKLET_SAMPLE);
+BLECharCharacteristic step_sample(BLE_UUID_CHAR, BLERead | BLEWrite);
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  //while (!Serial)
+    //;
   // put your setup code here, to run once:
   if (!BLE.begin()) {
     Serial.println("* Starting Bluetooth® Low Energy module failed!");
-    while (1);
+    while (1)
+      ;
   }
 
-  BLE.setLocalName("Nano 33 BLE (Central)"); 
+  BLE.setLocalName("Nano 33 BLE (Central)");
+  BLE.setAdvertisedService(Arduino_measurements);
+  Arduino_measurements.addCharacteristic(step_sample);
+  BLE.addService(Arduino_measurements);
+  step_sample.writeValue(0);
   BLE.advertise();
 
   Serial.println("Arduino Nano 33 BLE Sense (Central Device)");
@@ -21,20 +30,65 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-connectToPeripheral();
+  BLEDevice central = BLE.central();
+  Serial.println("- Discovering central device...");
+  delay(500);
+
+  if (central) {
+    Serial.println("* Connected to central device!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(central.address());
+    Serial.println(" ");
+
+    while (central.connected()) {
+      if (step_sample.written()) {
+        byte current_step = step_sample.value();
+        if (current_step & (1<<0)) {
+          Serial.println("Red");
+          Serial.println(" ");
+        }
+
+        if (current_step & (1<<1)) {
+          Serial.println("Blue");
+          Serial.println(" ");
+        }
+
+        if (current_step & (1<<2)) {
+          Serial.println("Green");
+          Serial.println(" ");
+        }
+
+        if (current_step & (1<<3)) {
+          Serial.println("Yellow");
+          Serial.println(" ");
+        }
+
+        if (current_step & (1<<5)) {
+          Serial.println("Done with calculating threshold values");
+          Serial.println(" ");
+        }
+
+        if (current_step & (1<<6)) {
+          Serial.println("Back to middle");
+          Serial.println(" ");
+        }
+      }
+    }
+
+    Serial.println("* Disconnected to central device!");
+  }
 }
 
-void connectToPeripheral(){
+void connectToPeripheral() {
   BLEDevice peripheral;
-  
+
   Serial.println("- Discovering peripheral device...");
 
-  do
-  {
+  do {
     BLE.scanForUuid(BLE_UUID_ANKLET_SAMPLE);
     peripheral = BLE.available();
   } while (!peripheral);
-  
+
   if (peripheral) {
     Serial.println("* Peripheral device found!");
     Serial.print("* Device MAC address: ");
@@ -49,10 +103,10 @@ void connectToPeripheral(){
   }
 }
 
-void step_detection(BLEDevice peripheral){
-   Serial.println("- Connecting to peripheral device...");
+void step_detection(BLEDevice peripheral) {
+  Serial.println("- Connecting to peripheral device...");
 
-   if (peripheral.connect()) {
+  if (peripheral.connect()) {
     Serial.println("* Connected to peripheral device!");
     Serial.println(" ");
   } else {
@@ -60,7 +114,7 @@ void step_detection(BLEDevice peripheral){
     Serial.println(" ");
     return;
   }
-  
+
   Serial.println("- Discovering peripheral device attributes...");
   if (peripheral.discoverAttributes()) {
     Serial.println("* Peripheral device attributes discovered!");
@@ -71,9 +125,9 @@ void step_detection(BLEDevice peripheral){
     peripheral.disconnect();
     return;
   }
-  
+
   BLECharacteristic step_Characteristic = peripheral.characteristic(BLE_UUID_CHAR);
-  
+
   if (!step_Characteristic) {
     Serial.println("* Peripheral device does not have step characteristic!");
     peripheral.disconnect();
@@ -81,10 +135,10 @@ void step_detection(BLEDevice peripheral){
   } else {
     step_Characteristic.subscribe();
   }
-  
-  while(peripheral.connected()){
-   // check if the value of the simple key characteristic has been updated
-    if (step_Characteristic.valueUpdated()){
+
+  while (peripheral.connected()) {
+    // check if the value of the simple key characteristic has been updated
+    if (step_Characteristic.valueUpdated()) {
       byte current_step;
       step_Characteristic.readValue(current_step);
 
@@ -94,23 +148,26 @@ void step_detection(BLEDevice peripheral){
       }
 
       if (current_step & 0x02) {
-        Serial.println("Yellow");
+        Serial.println("Blue");
         Serial.println(" ");
       }
 
-       if (current_step & 0x03) {
+      if (current_step & 0x03) {
         Serial.println("Green");
         Serial.println(" ");
       }
 
       if (current_step & 0x04) {
-        Serial.println("Blue");
+        Serial.println("Yellow");
+        Serial.println(" ");
+      }
+
+      if (current_step & 0x05) {
+        Serial.println("Done with calculating threshold values");
         Serial.println(" ");
       }
     }
-     //current_step = (char *)step_Characteristic.value();
-     
+    //current_step = (char *)step_Characteristic.value();
   }
   Serial.println("- Peripheral device disconnected!");
-
 }
